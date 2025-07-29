2b3b62af093d95cedc8d9e92acb0b8a1037658c9
Tejaswi711 <20x31a6651@gmail.com>
2025-07-29 14:02:57 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import PortfolioPage from './components/PortfolioPage';
+import PropertyList from './components/PropertyList';
+
+function App() {
+  return (
+    <div>
+      <PortfolioPage />
+      <hr />
+      <PropertyList />
+    </div>
+  );
+}
+
+export default App;
